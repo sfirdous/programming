b1ca3280f16f0eaf2ff2a6892c8cbae4a38ed7be
sfirdous <sfirdous0404@gmail.com>
2024-08-14 12:10:11 +0530

    count down from N to 1 using 3 loops

--- loops/count_down.c
+++ loops/count_down.c
@@ -4,9 +4,25 @@ int main()
     int n;
     printf("Enter the value of n ");
     scanf("%d",&n);
-    for(int i = n ; i > 0 ; --i)
-    {
-        printf("%d\t",i);
-    }
+    //for loop
+    // for(int i = n ; i > 0 ; --i)
+    // {
+    //     printf("%d\t",i);
+    // }
+
+    //while loop
+    // int i = n;
+    // while(i>0)
+    // {
+    //     printf("%d ",i);
+    //     --i;
+    // }
+
+    //do while loop
+    int i = n+1;
+    do{
+        printf("%d ",i);
+        --i;
+    }while(i > 0);
     return 0;
 }
