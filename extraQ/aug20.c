c45ba89505a55f66882cf1265fd49f1b5855817a
sfirdous <sfirdous0404@gmail.com>
2024-08-20 10:38:35 +0530

    Easy Questions Done

---
+++ extraQ/aug20.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main()
+{
+    
+    
+    // 1. **Print numbers from 1 to 10 using a `for` loop.**
+    for(int i = 1; i<10;++i)
+    {
+        printf("%d\n",i);
+
+    }
+}
