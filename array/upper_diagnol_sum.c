bb40aa60cd1da104a418e687873df23b8f895427
firdous <sfirdous0404@gmail.com>
2024-08-25 21:21:05 +0530

    Sum of upper diagnol matrix

---
+++ array/upper_diagnol_sum.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+int main()
+{
+    int n,m;
+    printf("Enter n and m: ");
+    scanf("%d%d",&n,&m);
+    int mat[n][m];
+    int sum = 0;
+    for (int row = 0;row < n; ++row)
+    {
+        for (int col = 0; col < m; ++col)
+        {
+            scanf("%d",&mat[row][col]);      
+        }
+        
+    }
+
+    for (int row = 0;row < n; ++row)
+    {
+        for (int col = 0; col < m; ++col)
+        {
+            printf("%d\t",mat[row][col]);      
+        }
+        printf("\n");
+    }
+    
+    for (int row = 0;row < n; ++row)
+    {
+        for (int col = 0; col < m; ++col)
+        {
+            if(row < col){
+                
+                sum += mat[row][col];
+                
+            }
+        }
+        
+    }
+    
+    printf("Sum : %d\n",sum);
+    
+}
