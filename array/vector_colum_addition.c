46eaa14e3f2b3ab9678b0b663514b6825da6a38d
sfirdous <sfirdous0404@gmail.com>
2024-08-26 14:08:55 +0530

    Added some print statements

--- array/vector_colum_addition.c
+++ array/vector_colum_addition.c
@@ -1,42 +1,47 @@
-#include<stdio.h>
+#include <stdio.h>
 
 int main()
 {
-    int n,m;
+    int n, m;
     printf("Enter n and m: ");
-    scanf("%d%d",&n,&m);
+    scanf("%d%d", &n, &m);
     int A[n][m];
-    int v[n][m];
+    int v[n][0];
+    int B[n][m];
 
+    printf("Entet Matrix A\n");
     for (int r = 0; r < n; r++)
     {
         for (int c = 0; c < m; c++)
         {
-            printf("Enter [%d][%d]: ",r,c);
-            scanf("%d",&A[n][m]);
+            printf("Enter [%d][%d]: ", r, c);
+            scanf("%d", &A[r][c]);
         }
-        
     }
 
+    printf("Enter vector v\n");
     for (int r = 0; r < n; r++)
     {
-        for (int c = 0; c < m; c++)
-        {
-            printf("Enter [%d][%d]: ",r,c);
-            scanf("%d",&v[n][m]);
-        }
-        
+
+        printf("Enter [%d][0]: ", r);
+        scanf("%d", &v[r][0]);
     }
 
     for (int c = 0; c < m; c++)
     {
         for (int r = 0; r < n; r++)
         {
-        
+            B[r][c] = v[r][0] + A[c][r];
         }
-        
     }
-
-
-    
+ 
+    printf("Matrix B\n");
+    for (int r = 0; r < n; r++)
+    {
+        for (int c = 0; c < m; c++)
+        {
+            printf("%d\t", B[r][c]);
+        }
+        printf("\n");
+    }
 }
