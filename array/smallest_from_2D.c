1cdefeca312a03cd63e14e962a54a5aa05045cdc
firdous <sfirdous0404@gmail.com>
2024-08-29 21:00:10 +0530

    Finding the smallest element form martrix of size nXm

--- array/smallest_from_2D.c
+++ array/smallest_from_2D.c
@@ -1,27 +1,42 @@
-#include<stdio.h>
+#include <stdio.h>
 
 int main()
 {
-    int n,m;
+    int n, m;
     printf("Enter n and m: ");
-    scanf("%d%d",&n,&m);
+    scanf("%d%d", &n, &m);
     int mat[n][m];
-    
-    for (int row = 0;row < n; ++row)
+
+    for (int row = 0; row < n; ++row)
     {
         for (int col = 0; col < m; ++col)
         {
-            scanf("%d",&mat[row][col]);      
+            scanf("%d", &mat[row][col]);
         }
-        
     }
 
-    for (int row = 0;row < n; ++row)
+    for (int row = 0; row < n; ++row)
     {
         for (int col = 0; col < m; ++col)
         {
-            printf("%d\t",mat[row][col]);      
+            printf("%d\t", mat[row][col]);
         }
         printf("\n");
     }
+
+    int smallest = mat[0][0];
+    for (int row = 0; row < n; ++row)
+    {
+        for (int col = 0; col < m; ++col)
+        {
+            
+            if (smallest > mat[row][col])
+            {
+                smallest = mat[row][col];
+            }
+            
+        }
+    }
+
+    printf("The smallest element is %d\n",smallest);
 }
