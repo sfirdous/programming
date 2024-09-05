9cf081e62ceac71ef8ff2bcc979c5c3184164a9f
sfirdous <sfirdous0404@gmail.com>
2024-09-05 13:47:28 +0530

    FlowerBed accepting the plotted flowers

--- test_2/flowerbed.c
+++ test_2/flowerbed.c
@@ -1,5 +1,19 @@
-#include<stdio.h>
+#include <stdio.h>
+
+bool flowerbed()
+{
+}
 int main()
 {
-    
+    int n;
+    printf("Enter the size of flowerbed: ");
+    scanf("%d", &n);
+
+    int flowerbed[n];
+
+    for (int i = 0; i < n; ++i)
+    {
+        printf("Flower : 1 \t No Flower : 0 ");
+        scanf("%d",&flowerbed[i]);
+    }
 }
