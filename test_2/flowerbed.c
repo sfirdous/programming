417d36ba46691a0fdbd3bfdf8be4457e650529d5
sfirdous <sfirdous0404@gmail.com>
2024-09-06 08:58:08 +0530

    Defined some part of flowerbed function

--- test_2/flowerbed.c
+++ test_2/flowerbed.c
@@ -1,11 +1,29 @@
 #include <stdio.h>
 #include<stdbool.h>
-bool flowerbed(int flowerbed[],int n)
+bool flowerbed(int flowerbed[],int n,int new_f)
 {
+    for (int i = 0; i < n; ++i)
+    {
+        if (flowerbed[i] == 0)
+        {
+            bool left;
+            bool right;
+            if(i == 0 || flowerbed[i-1] == 0){
+                left = true;
+            }
+            if (i == n || flowerbed[i+1]){
+                right = true;
+            }
+            
+            
+        }
+        
+    }
+    
 }
 int main()
 {
-    int n;
+    int n,new_f;
     printf("Enter the size of flowerbed: ");
     scanf("%d", &n);
 
@@ -16,6 +34,11 @@ int main()
         printf("Flower : 1 \t No Flower : 0 ");
         scanf("%d",&flowerbed[i]);
     }
+
+    printf("Enter number of new flowers to be plotted: ");
+    scanf("%d",&new_f);
+
+
 
     printf("\n");
     for (int i = 0; i < n; ++i)
