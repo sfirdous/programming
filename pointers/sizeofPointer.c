467a1af8311f897391ca82e3a58e96388d78aa1d
sfirdous <sfirdous0404@gmail.com>
2024-09-11 08:32:36 +0530

    Size of different datatype of pointer

---
+++ pointers/sizeofPointer.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main()
+{
+    printf("Size of integer Pointer %d\n",sizeof(int *));
+    printf("Size of float Pointer %d\n",sizeof(float *));
+    printf("Size of double Pointer %d\n",sizeof(int *));
+    printf("Size of unsigned long Pointer %d\n",sizeof(unsigned long *));
+    printf("Size of void Pointer %d\n",sizeof(void *));
+}
