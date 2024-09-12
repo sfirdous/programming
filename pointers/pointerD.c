f9fb6f35f28ce368ee791906d688e083da791884
sfirdous <sfirdous0404@gmail.com>
2024-09-12 13:43:38 +0530

    Updated pointerD

--- pointers/pointerD.c
+++ pointers/pointerD.c
@@ -5,4 +5,11 @@ int main()
     int i = 10;
     int *p = &i;
     printf("i = %d p = %d \n",i,p);
+
+    printf("addres of i using & %d\n",&i); 
+    printf("Address of pointer variable : %d\n",&p);
+
+    ++*p;
+    printf("i = %d p = %d \n",i,p);
+
 }
