234c9d19bb75fee827897b8269fea18e3cdd2024
Shaikh Firdous <126231373+sfirdous@users.noreply.github.com>
2024-09-20 14:24:06 +0530

    Update unique element.c

--- test_3/unique element.c
+++ test_3/unique element.c
@@ -1,11 +1,13 @@
-#include <stdio.h>
+#include<stdio.h>
 #include<stdlib.h>
+#include<stdbool.h>
 
 int main() {
    int n;
+   bool once = false;
    printf("Enter size of nums: ");
    scanf("%d",&n);
-   
+
    int *nums = calloc(n,sizeof(n));
    printf("Enter array elements\n");
    //using while loop
@@ -14,7 +16,29 @@ int main() {
        scanf("%d",&nums[i]);
        i++;
    }
-    
+   
+   int j = 0;
+   int k;
+   while(j<n){
+       k = 0;
+       while(k<n){
+           if(j != k && nums[j]!=nums[k]){
+              once= true;
+              break;
+           }
+           k++;
+       }
+       if(once){
+           break;
+       }
+       j++;
+   }
+   
+   if(once){
+       printf("%d\n",nums[k]);
+   }
+   
+
 
     return 0;
 }
