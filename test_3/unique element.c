239b82fe77f13e9a86146a4359fb18437d23e03d
Shaikh Firdous <126231373+sfirdous@users.noreply.github.com>
2024-09-20 14:01:28 +0530

    Created a dynamic Array called nums

--- test_3/unique element.c
+++ test_3/unique element.c
@@ -1,7 +1,16 @@
 #include <stdio.h>
+#include<stdlib.h>
 
 int main() {
+   int n;
+   printf("Enter size of nums: ");
+   scanf("%d",&n);
    
+   int *nums = calloc(n,sizeof(n));
+   printf("Enter array elements\n");
+   for(int i = 0; i< n;++i){
+       scanf("%d",&nums[i]);
+   }
     
 
     return 0;
