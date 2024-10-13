9837e0cfd56453760dc51746d13f627015fa3dd0
firdous <sfirdous0404@gmail.com>
2024-10-13 13:04:06 +0530

    A number that is min from its row and max from its columns is Lucky Number

--- test_3/luckyNumber.c
+++ test_3/luckyNumber.c
@@ -2,66 +2,77 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
+void matrixAllocation(int **mat, int rows, int cols)
+{
+
+    for (int i = 0; i < rows; ++i)
+    {
+        mat[i] = calloc(cols, sizeof(int));
+    }
+}
+
+void matrixDeallocation(int **mat, int rows, int cols)
+{
+    for (int i = 0; i < rows; i++)
+    {
+        free(mat[i]);
+    }
+    free(mat);
+}
+
+void matrixAccept(int **mat, int rows, int cols)
+{
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            scanf("%d", &mat[i][j]);
+        }
+    }
+}
+
+void displayLucky(int **mat, int rows, int cols)
+{
+}
+
 int main()
 {
     int rows, cols;
-    int min_row, max_col;
-    printf("Enter rows and cols of matrix: ");
+
+    printf("Enter the number of rows and columns: ");
     scanf("%d%d", &rows, &cols);
-    // int **mat = (int*)malloc(rows*sizeof(int)); // using malloc;
-    int **mat = calloc(rows, sizeof(int)); // using calloc
 
-    for (int r = 0; r < rows; r++)
+    int **mat = (int **)malloc(rows * sizeof(int));
+
+    matrixAllocation(mat, rows, cols);
+    printf("Enter matrix\n");
+    matrixAccept(mat, rows, cols);
+    // displayLucky(mat,rows,cols);
+
+
+    int min,max;
+    
+    for (int i = 0; i < rows; i++)
     {
-        mat[r] = calloc(cols, sizeof(int)); // using calloc
-        if (mat[r] == NULL)
+        for (int j = 0; j < cols; j++)
         {
-            printf("error\n");
-            for (int i = 0; i < rows; i++)
+            min = max = mat[i][j];
+            for (int k = 0; k < cols ; k++)
             {
-                free(mat[i]);
+                if (mat[i][k] < min)
+                    min = mat[i][k];
+                // printf("%d\t", min);
             }
-            free(mat);
-            exit(0);
-        }
-    }
 
-    printf("Enter array elements\n");
-    for (int r = 0; r < rows; r++)
-    {
-        for (int c = 0; c < cols; c++)
-        {
-            scanf("%d", &mat[r][c]);
-        }
-    }
-
-    int row_min, col_max = mat[0][0];
-    for (int r = 0; r < rows; ++r)
-    {
-        for (int c = 0; c < cols; ++c)
-        {
-            min_row = mat[r][0];
-            for (int i = 0; i < cols; i++)
+            for (int l = 0; l < rows; l++)
             {
-                if (min_row > mat[r][i])
-                {
-                    min_row = mat[r][i];
-                }
+                if (mat[l][j] > max)
+                    max = mat[l][j];
+                // printf("%d\n", max);
             }
 
-            max_col = mat[0][c];
-            for (int i = 0; i < rows; i++)
-            {
-                if (max_col < mat[i][c])
-                {
-                    max_col = mat[i][c];
-                }
-                
-            }
-
-            if(max_col == min_row){
-                printf("lucky: %d\n",min_row);
-            }
+            if (min == max)
+                printf("lucky %d", min);
         }
     }
 }
